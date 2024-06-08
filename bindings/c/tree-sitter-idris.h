29c8fc8679d5fb7bfce6eb7b0cc1299c523cbf06
Hideaki Kawai <kayhide@gmail.com>
2024-06-08 17:20:32 +0900

    Generate

---
+++ bindings/c/tree-sitter-idris.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_IDRIS_H_
+#define TREE_SITTER_IDRIS_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_idris(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_IDRIS_H_
